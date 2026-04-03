9acc71757805114861672853b310ca0ec2cee419
ahmadrojahakam1-design <ahmadrojahakam1@gmail.com>
2026-04-03 19:25:48 +0700

    Fix output formatting in mainTest.java

--- mainTest.java
+++ mainTest.java
@@ -12,7 +12,8 @@ public class mainTest {
         System.out.println();
 
         pekerja c = new pekerja(1000, 2016, 3, 2, 4, "C", "111", true, true);
-        System.out.println(c + "\n");
+        System.out.println(c);
+        System.out.println();
 
         manager d = new manager("HRD", 1000, 2017, 1, 2, 3, "D", "111", true, true);
         System.out.println(d);
